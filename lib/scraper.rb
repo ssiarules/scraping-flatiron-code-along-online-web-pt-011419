19601c36f1e956933f4d3ed7df24142e6b550cc6
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-22 06:51:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,13 +4,15 @@ require 'pry'
 
 require_relative './course.rb'
 
-def get_page
+
+
+class Scraper
+  
+  def get_page
   doc = Nokogiri::HTML (open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   
   binding.pry
 end 
-
-class Scraper
   
     def print_courses
     self.make_courses
